04c6ef353697e1e38f4fddc71e5d6feef3346776
disha patel <disha.p@hotmail.co.uk>
2016-08-17 17:37:31 +0100

    tried to make it a little prettier

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,5 +3,5 @@ class Restaurant < ApplicationRecord
   CATEGORIES = %w(chinese italian japanese french belgian)
   has_many :reviews, dependent: :destroy
   validates :name, :address, :phone_number, presence: true
-  validates :category, inclusion: { in: CATEGORIES, allow_nil: false}
+  validates :category, inclusion: { in: CATEGORIES, allow_nil: false}, presence: true
 end
